e678b5cf8bc9b72bdbb47a0cfc4b5064677e3673
Ahnaf-dev <ahnafbari99@gmail.com>
2022-11-08 17:18:22 -0700

    added accesibility for screenreader to work with modal

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function generateModal() {
     "Clean code principles lead to source code that's highly modular and thus easier to read and test. If you think of these practices as part of a house, clean code is the foundation. Implementing clean code principles is a foundational skill that pays off especially well when it's time to refactor code or bring code under test";
   const modal = createModal(modalContent);
   document.body.append(modal);
+  setFocus(returnNodeByClass(".modal__content"));
   setTimeout(() => resetCoordsAndScale(modal), 100);
 }
 
@@ -18,14 +19,25 @@ function createModal(modalContent) {
   const modal = document.createElement("div");
   modal.className = "modal center";
 
+  const closeModal = () => {
+    modal.remove();
+    setFocus(openModalButton);
+  };
+
   const modalContentContainer = document.createElement("div");
   modalContentContainer.className = "modal__content";
+  modalContentContainer.ariaModal = true;
+  modalContentContainer.role = "dialog";
+  modalContentContainer.tabIndex = 0;
 
+  modalContentContainer.addEventListener("keyup", (e) =>
+    e.keyCode === 27 ? closeModal() : null
+  );
   const closeModalBtn = document.createElement("button");
   closeModalBtn.className = "modal__close btn";
   closeModalBtn.innerText = "Close Modal";
+  closeModalBtn.ariaLabel = "press ESC to close modal";
 
-  const closeModal = () => modal.remove();
   closeModalBtn.addEventListener("click", closeModal);
 
   const modalText = document.createElement("p");
@@ -52,4 +64,8 @@ function resetCoordsAndScale(elem) {
   elem.style.left = "0";
   elem.style.top = "0";
   elem.style.transform = "scaleX(100%)";
+}
+
+function setFocus(elem) {
+  elem.focus();
 }
