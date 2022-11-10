25dcfb2b77cf1cdde5fca6a34aa080db7515315c
Ahnaf-dev <ahnafbari99@gmail.com>
2022-11-10 10:18:19 -0700

    removed data

--- data/modalContent.js
+++
@@ -1,4 +0,0 @@
-const modalContent =
-  "Clean code principles lead to source code that's highly modular and thus easier to read and test. If you think of these practices as part of a house, clean code is the foundation. Implementing clean code principles is a foundational skill that pays off especially well when it's time to refactor code or bring code under test";
-
-export default modalContent;
